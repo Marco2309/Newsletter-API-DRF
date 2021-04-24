ba362bec1b113648560026182ed8104e5fce1d85
Marco R <trujas_2309@hotmail.com>
2021-04-24 14:06:13 -0500

    primeras relaciones

--- newsletter_app/models.py
+++ newsletter_app/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from tags_app.models import Tag
+from users_app.models import User
 
 
 class Newsletters(models.Model):
@@ -7,6 +9,9 @@ class Newsletters(models.Model):
     imagen = models.ImageField(upload_to='imagenes', null=True)
     target = models.IntegerField
     frecuencia = models.CharField(max_length=50)
+    tags = models.ManyToManyField(Tag, related_name='newsletters')
+    user = models.ForeignKey(User, related_name='user',
+                             on_delete=models.SET_NULL, null=True)
     fecha_creacion = models.DateField()
     created_at = models.DateTimeField()
     update_at = models.DateTimeField()
