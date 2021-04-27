b343ba6a131683237a6b09b4c3056f246bfafd5b
juan correa <juansecorrea12@gmail.com>
2021-04-27 13:02:02 -0500

    tests newsletter

--- newsletter_app/tests.py
+++ newsletter_app/tests.py
@@ -10,7 +10,7 @@ class TestNewsLetter(APITestCase):
         self.tag = Tag.objects.create(
             nombre = 'tag 1',
             slug = 'slug 1',
-            fecha_creacion = '2021-04-27T17:21:01.757261Z'
+            fecha_creacion = '2021-04-27'
         )
         self.user1 = User.objects.create(
             is_administrador = True,
@@ -29,9 +29,24 @@ class TestNewsLetter(APITestCase):
             fecha_creacion = '2020-04-06'
         )
         self.newsletter.tags.add(self.tag)
-        # self.newsletter.user.add(self.user1)
         
     def test_get_newsletter(self):
         response = self.client.get(f'{self.host}/newsletter/')
         self.assertEqual(response.status_code, 200)
+        self.assertEqual(Newsletters.objects.all().count(), 1)
+        self.assertEqual(len(response.data), 1)
+        
+    def test_post_newsletter(self):
+        data = {
+            'nombre' : 'newsletter 2',
+            'description' : 'description 2',
+            'imagen' : 'newsletter-2.png',
+            'target': 2,
+            'frecuencia' : 'frecuencia 2',
+            'tags' : [self.tag.id],
+            'fecha_creacion' : '2020-04-06'
+        }
+        response = self.client.post(f'{self.host}/newsletter/', data)
+        self.assertEqual(response.status_code, 400, response.data)
+        self.assertEqual(Newsletters.objects.all().count(), 1)
         
