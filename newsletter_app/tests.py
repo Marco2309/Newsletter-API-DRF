b17905b3aac480ec8d35919a74a611980bbea6c5
juan correa <juansecorrea12@gmail.com>
2021-04-27 12:32:56 -0500

    add some test

--- newsletter_app/tests.py
+++ newsletter_app/tests.py
@@ -10,14 +10,15 @@ class TestNewsLetter(APITestCase):
         self.tag = Tag.objects.create(
             nombre = 'tag 1',
             slug = 'slug 1',
-            fecha_creacion = '2021-03-01',
-            created_at = '2021-03-01',
-            update_at = '2021-01-02'
+            fecha_creacion = '2021-04-27T17:21:01.757261Z'
         )
-        self.user = User.objects.create(
-            username='juancorrea',
-            password = 'jusecovi11',
-            is_administrador = True
+        self.user1 = User.objects.create(
+            is_administrador = True,
+            username='prueba 1',
+            password = '12345',
+            email = 'prueba@mail.com',
+            first_name = 'prueba',
+            last_name = 'prueba'
         )
         self.newsletter = Newsletters.objects.create(
             nombre = 'newsletter 1',
@@ -27,13 +28,10 @@ class TestNewsLetter(APITestCase):
             frecuencia = 'frecuencia 1',
             fecha_creacion = '2020-04-06'
         )
-        self.newsletter.tags.add(self.tags)
-        self.newsletter.user.add(self.user)
+        self.newsletter.tags.add(self.tag)
+        # self.newsletter.user.add(self.user1)
         
     def test_get_newsletter(self):
         response = self.client.get(f'{self.host}/newsletter/')
         self.assertEqual(response.status_code, 200)
         
-'''Agregar el tema del target al modelo de la base de datos 
-verificar que exista en la db para realizar la prueba 
-'''
