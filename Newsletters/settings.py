b3fb26055776889d45a2feaaafe22dba004ed5ac
Marco R <trujas_2309@hotmail.com>
2021-04-27 14:54:06 -0500

    problema solucionado

--- Newsletters/settings.py
+++ Newsletters/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-AUTH_USER_MODEL = 'users_app.User'
+# AUTH_USER_MODEL = 'users_app.User'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
