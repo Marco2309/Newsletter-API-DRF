437940fa1941acd6cd5c5bfac78a9797e18b6081
JuanCarlosLP <jclarios.js@gmail.com>
2021-04-28 21:48:52 -0500

    Auth test

--- users_app/tests.py
+++ users_app/tests.py
@@ -6,20 +6,26 @@ from users_app.models import User
 class TestLoginRegister(APITestCase):
     def setUp(self):
         self.host = 'http://127.0.0.1:8000'
+        self.user = User.objects.create(
+            username= 'user1',
+            password= 'password1234',
+            email= 'email@gmail.com',
+            first_name= 'John',
+            last_name= 'Doe'
+        )
 
     def test_register_user(self):
         data = {
-            'username': 'user1',
+            'username': 'user2',
             'password': 'password1234',
             'email': 'email@gmail.com',
             'first_name': 'John',
-            'last_name': 'Doe',
-            'is_administrador': False
+            'last_name': 'Doe'
         }
         response = self.client.post(f'{self.host}/user/', data)
 
         self.assertEqual(response.status_code, 201, response.data)
-        self.assertEqual(User.objects.all().count(), 1)
+        self.assertEqual(User.objects.all().count(), 2)
 
     def test_register_admin(self):
         data = {
@@ -27,30 +33,27 @@ class TestLoginRegister(APITestCase):
             'password': 'password1234',
             'email': 'email@gmail.com',
             'first_name': 'John',
-            'last_name': 'Doe',
-            'is_administrador': True
+            'last_name': 'Doe'
         }
         response = self.client.post(f'{self.host}/user/admin/', data)
 
         self.assertEqual(response.status_code, 201, response.data)
-        self.assertEqual(User.objects.all().count(), 1)
+        self.assertEqual(User.objects.all().count(), 2)
 
     def test_login_user(self):
         data = {
             'username': 'user1',
             'password': 'password1234',
         }
-        response = self.client.post(f'{self.host}/user/1', data)
+        response = self.client.post(f'{self.host}/api/token/', data)
 
-        self.assertEqual(response.status_code, 302, response.data)
-        self.assertEqual(User.objects.all().count(), 1)
+        self.assertEqual(response.status_code, 200, response.data)
 
     def test_login_admin(self):
         data = {
             'username': 'user1',
             'password': 'password1234',
         }
-        response = self.client.post(f'{self.host}/user/admin/1', data)
+        response = self.client.post(f'{self.host}/api/token/', data)
 
-        self.assertEqual(response.status_code, 302, response.data)
-        self.assertEqual(User.objects.all().count(), 1)
+        self.assertEqual(response.status_code, 200, response.data)
