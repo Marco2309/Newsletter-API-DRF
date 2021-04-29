13ecd459cafdfa5f84e219d4028111b3fae0551e
juan correa <juansecorrea12@gmail.com>
2021-04-29 15:30:57 -0500

    add test for tags

--- newsletter_app/tests.py
+++ newsletter_app/tests.py
@@ -1,33 +1,19 @@
-# import os
-# import io
-
 from rest_framework.test import APITestCase
 from newsletter_app.models import Newsletters
 from tags_app.models import Tag
-from users_app.models import User
+from django.contrib.auth.models import User
 from rest_framework.test import force_authenticate
-# from PIL import Image
 
 class TestNewsLetter(APITestCase):
     
     def setUp(self):
-        
-        # super().setUp()
-        # self.newsletter.imagen = tempfile.NamedTemporaryFile(suffix='.jpg')
-        # image = Image.new('RGB', (100, 100))
-        # image.save(self.newsletter.imagen.name)
-        # self.params = {
-        #     'photo': self.newsletter.imagen
-        # }
-        
         self.host = 'http://127.0.0.1:8000'
         self.tag = Tag.objects.create(
             nombre = 'tag 1',
             slug = 'slug 1',
             fecha_creacion = '2021-04-27'
         )
-        self.user1 = User.objects.create(
-            is_administrador = True,
+        self.user = User.objects.create(
             username='prueba 1',
             password = '12345',
             email = 'prueba@mail.com',
@@ -39,42 +25,37 @@ class TestNewsLetter(APITestCase):
             description = 'description 1 ',
             target = 1,
             frecuencia = 'frecuencia 1',
-            fecha_creacion = '2020-04-06'
+            fecha_creacion = '2020-04-06',
         )
         self.newsletter.tags.add(self.tag)
+        self.newsletter.users.add(self.user)
+        self.newsletter.members.add(self.user)
+        self.newsletter.voters.add(self.user)
         
     def test_get_newsletter(self):
         response = self.client.get(f'{self.host}/newsletter/')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(Newsletters.objects.all().count(), 1)
         self.assertEqual(len(response.data), 1)
-        
-    
-    # def test_valid_authenticated_post_returns_201(self):
-    #     response = self.auth_client.post(
-    #         reverse(self.view_name), data=self.params, format='multipart')
-    
-    #     self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-    
-    
     
     def test_post_newsletter(self):
         data = {
             'nombre' : 'newsletter 2',
             'description' : 'description 2',
-            # 'imagen' : self.generate_photo_file(),
             'target': 2,
             'frecuencia' : 'frecuencia 2',
             'tags' : [self.tag.id],
             'fecha_creacion' : '2020-04-06',
-            'user': self.user1.id
+            'user': self.user.id,
+            'users': [self.user.id],
+            'members': [self.user.id],
+            'voters': [self.user.id]
         }
         response = self.client.post(f'{self.host}/newsletter/', data, format='json')
         self.assertEqual(response.status_code, 201, response.data)
         self.assertEqual(Newsletters.objects.all().count(), 2)
         self.assertEqual(Newsletters.objects.get(id=1).nombre, 'newsletter 1')
-        # print(response.data)
-        self.assertEqual(len(response.data), 11)
+        self.assertEqual(len(response.data), 14)
     
     def test_post_logIn_authenticated(self):
         data = {
@@ -84,9 +65,12 @@ class TestNewsLetter(APITestCase):
             'frecuencia' : 'frecuencia 3',
             'tags' : [self.tag.id],
             'fecha_creacion' : '2020-04-06',
-            'user': self.user1.id
+            'user': self.user.id,
+            'users': [self.user.id],
+            'members': [self.user.id],
+            'voters': [self.user.id]
         }
-        self.client.force_authenticate(self.user1)
+        self.client.force_authenticate(self.user)
         response = self.client.post(f'{self.host}/newsletter/', data)
         self.assertEqual(response.status_code, 201)
         
@@ -99,7 +83,10 @@ class TestNewsLetter(APITestCase):
             'frecuencia' : 'frecuencia actualizado',
             'tags' : [self.tag.id],
             'fecha_creacion' : '2020-04-06',
-            'user': self.user1.id
+            'user': self.user.id,
+            'users': [self.user.id],
+            'members': [self.user.id],
+            'voters': [self.user.id]
         }
         nombre_newsletter = Newsletters.objects.get(id=self.newsletter.id).nombre
         response = self.client.put(f'{self.host}/newsletter/{self.newsletter.id}/',data)
--- tags_app/tests.py
+++ tags_app/tests.py
@@ -1,3 +1,43 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from tags_app.models import Tag
 
-# Create your tests here.
+class TestTags(APITestCase):
+    
+    def setUp(self):
+        self.host = 'http://127.0.0.1:8000'
+        self.tag = Tag.objects.create(
+            nombre = 'tag 1',
+            slug = 'slug 1',
+            fecha_creacion = '2021-04-27'
+        )    
+    
+    def tes_get_tags(self):
+        response = self.client.get(f'{self.host}/tags/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(Tag.objects.all().count(), 1)
+    
+    def test_post_tags(self):
+        data={
+            "nombre" : 'tag 2',
+            "slug" : 'slug 2',
+            "fecha_creacion" : '2021-04-27'
+        }
+        response = self.client.post(f'{self.host}/tags/', data)
+        self.assertEqual(response.status_code, 201, response.data)
+        self.assertEqual(Tag.objects.get(id=2).nombre, 'tag 2')
+        
+    def test_put_tags(self):
+        data={
+            "nombre" : 'tag actualizado',
+            "slug" : 'slug actualizado',
+            "fecha_creacion" : '2021-04-26'
+        }
+        nombreTag = Tag.objects.get(id = self.tag.id).nombre
+        response = self.client.put(f'{self.host}/tags/{self.tag.id}/',data)
+        self.assertEqual(response.status_code, 200, response.data)
+        self.assertNotEqual(nombreTag, data['nombre'])
+        
+    def test_delete_tags(self):
+        response = self.client.delete(f'{self.host}/tags/{self.tag.id}/')
+        self.assertEqual(len(Tag.objects.all()), 0)
+        
